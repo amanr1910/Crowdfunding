8b415bdb5da70570505b1372578da2eb8e506779
Aman Raj <92713433+amanr1910@users.noreply.github.com>
2023-06-29 14:08:33 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['crowdfunding.infura-ipfs.io'],
+  },
+}
+
+// module.exports = nextConfig
